8484d0a13c00d813fbb6c0c310ba66dab71b4bfc
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-02-23 18:24:56 +0200

    Removed unnecessary code

--- src/js/removeBookFromCart.js
+++ src/js/removeBookFromCart.js
@@ -1,26 +1,20 @@
 import { setToLocalStorage, getFromLocalStorage } from './localStorageAPI';
 
+import { refsLS } from './keyConstsLS';
+
+let booksInCart = refsLS.booksInCart;
+
 function removeBookFromCart(bookId) {
   //Get arr from Local Storage
-  let booksInCart = getFromLocalStorage('booksInCart');
+  booksInCart = getFromLocalStorage('booksInCart');
 
   if (booksInCart === null) {
     return;
   }
-  //Find the index of book for it`s id
-  const index = booksInCart.findIndex(book => book._id === bookId);
-
-  //if book doesn`t fnd
-  if (index === -1) {
-    console.log(`The book with id: "${bookId}" was not found`);
-    return;
-  }
-  //Delete book from the arr by it`s index
-  booksInCart.splice(index, 1);
+  //Filtering and checking the unique books
+  const newBook = booksInCart.filter(book => book._id !== bookId);
 
   //Update data in Local Storage
-  setToLocalStorage('booksInCart', booksInCart);
+  setToLocalStorage('booksInCart', newBook);
   console.log(`The book with id ${bookId} was remove from cart`);
 }
-
-removeBookFromCart('642fd89ac8cf5ee957f12361');
--- src/main.js
+++ src/main.js
@@ -1,2 +1,1 @@
-import './js/addBookToCart';
-import './js/removeBookFromCart';
+
