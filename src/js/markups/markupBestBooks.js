36a81737d46e41fcb45bf69a9fcf44b67d5d0e46
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-02-23 17:14:39 +0200

    Create markupBestBooks.js

---
+++ src/js/markups/markupBestBooks.js
@@ -0,0 +1,13 @@
+export function createBestSellersBooksMarkup(list_name, books) {
+  const categoryItem = document.createElement('li');
+  categoryItem.classList.toggle('category-books-item');
+  const categoryTitle = document.createElement('h3');
+  categoryTitle.classList.toggle('category-title');
+  categoryTitle.textContent = `${list_name}`;
+  const categoryList = document.createElement('ul');
+  categoryList.textContent = `${books.map(createBookCardMarkup).join('')}`;
+  const categoryButton = document.createElement('button');
+  categoryButton.classList.toggle('category-button');
+  categoryButton.setAttribute('type', 'button');
+  categoryButton.textContent = 'see more';
+}
