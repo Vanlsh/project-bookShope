b622f7b2d7337d62610b6db941969cd3ee3007e2
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-02-24 17:33:42 +0200

    styles

--- src/js/markups/markupBasketShList.js
+++ src/js/markups/markupBasketShList.js
@@ -6,34 +6,34 @@ start.addEventListener('click', renderBasketItem);
 
 async function renderBasketItem() {
   const data = await getSelectedCategory('Paperback Nonfiction');
-
   console.log(data);
-
-  data.forEach(book => {
-    const markup = markupBasketItem(
-      book.book_image,
-      book.title,
-      book.category,
-      book.description,
-      book.author
-    );
-    basketShoppingList.insertAdjacentHTML('beforeend', markup);
-  });
+  const books = data.map(markupBasketItem).join();
+  basketShoppingList.innerHTML = books;
 }
 
-function markupBasketItem(book_image, title, category, description, author) {
+function markupBasketItem({
+  book_image,
+  title,
+  list_name,
+  description,
+  author,
+}) {
   return `
     <li class="basket-item">
       <img class="basket-item-img" src="${book_image}" alt="${title}"/>
-      <div>
-<div>
-    <div>
+      <div class="basket-item-info">
+<div class="first-item-part">
+    <div class="title-category-container">
     <h2 class="title-basket-item">${title}</h2>
-    <p class="category-basket-item">${category}</p>
+    <p class="category-basket-item">${list_name}</p>
     </div>    
-    <button class="delete-basket-item-btn"></button>
+    <button class="delete-basket-item-btn">
+      <svg class="trash-btn-icon" height="16" width="16">
+                <use href="../img/icons.svg#icon-trash"></use>
+              </svg></button>
 </div>
     <p class="descr-basket-item">${description}</p>
+    <div class="last-item-part">
     <p class="author-basket-item">${author}</p>
      <ul class="links-shop-list">
      <li class="link-shop-item">
@@ -44,6 +44,20 @@ function markupBasketItem(book_image, title, category, description, author) {
       </li>
      </ul>
      </div>
-    </li>
-  `;
+     </div>
+    </li>`;
 }
+
+const titleParagraphs = document.querySelectorAll('.title-basket-item');
+titleParagraphs.forEach(paragraph => {
+  const text = paragraph.textContent;
+  const lineHeight = parseInt(getComputedStyle(paragraph).lineHeight);
+  const maxHeight = lineHeight * 1;
+  if (paragraph.offsetHeight > maxHeight) {
+    let index = Math.floor(maxHeight / lineHeight);
+    while (paragraph.offsetHeight > maxHeight) {
+      index--;
+      paragraph.textContent = text.split(' ').slice(0, index).join(' ') + '...';
+    }
+  }
+});
