33b4e659025b8bf44a008b2346a31e07c95cb415
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-02-26 01:45:00 +0200

    updated the operation of the menu by adding appropriate classes

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,14 +1,37 @@
-export function toggleMenu() {
-    let menu = document.querySelector('.burger-wraper');
-    let burgerIcon = document.querySelector('.header-open-button');
+  const openButton = document.querySelector('.header-open-button');
+  const closeButton = document.querySelector('.header-close-button');
+  const menuWrapper = document.querySelector('.burger-wraper');
+  const headerContainer = document.querySelector('.header-container');
+  const body = document.querySelector('body');
 
-    if (menu.classList.contains('is-closed')) {
-        menu.classList.remove('is-closed');
-        menu.classList.add('is-open');
-        burgerIcon.innerHTML = '<svg class="header-close-icon" width="28" height="28"><use href="../img/icons.svg#icon-x-close"></use></svg>';
-    } else {
-        menu.classList.remove('is-open');
-        menu.classList.add('is-closed');
-        burgerIcon.innerHTML = '<svg class="header-close-icon" width="28" height="28"><use href="../img/icons.svg#icon-align-left"></use></svg>';
-    }
-}
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  openButton.addEventListener('click', function() {
+    menuWrapper.classList.add('menu-is-open');
+    headerContainer.classList.add('is-open');
+    body.style.overflow = 'hidden'; 
+
+   
+    headerContainer.style.backgroundColor = 'var(--item-background-color)';
+  });
+
+  closeButton.addEventListener('click', function() {
+    menuWrapper.classList.remove('menu-is-open');
+    headerContainer.classList.remove('is-open');
+    body.style.overflow = '';
+  });
+
+  closeButton.addEventListener('click', function() {
+    menuWrapper.classList.replace('menu-is-open', 'menu-is-closed');
+  });
+
+  const menuLinks = document.querySelectorAll('.burger-list a');
+  menuLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      menuWrapper.classList.remove('menu-is-open');
+      headerContainer.classList.remove('is-open');
+      body.style.overflow = '';
+    });
+  });
+});
