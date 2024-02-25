2626dbeda9164df2d99ad6f1efeac98af851fad6
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-02-25 20:04:07 +0200

    Changed styles and rename function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/renders/renderNavCategoriesList';
-import { getSpecificCategory } from './js/getSpecificNameCategory';
+import { handlerGalleryNav } from './js/handlerGalleryNav';
 import { refs } from './js/refs';
 
 const { ulCategoryContainer } = refs;
 
-ulCategoryContainer.addEventListener('click', getSpecificCategory);
+ulCategoryContainer.addEventListener('click', handlerGalleryNav);
