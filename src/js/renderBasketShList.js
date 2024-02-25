4b1175dfcef1aa823c176f4ad739852f582ffcb9
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-02-25 18:20:47 +0200

    1

--- src/js/renderBasketShList.js
+++ src/js/renderBasketShList.js
@@ -1,13 +1,34 @@
 import { getSelectedCategory } from './api/books';
 import { markupBasketItem } from './markups/markupBasketShList';
+import { addBookToCart } from './addBookToCart';
+import { refsLS } from './keyConstsLS';
+import { getFromLocalStorage } from './localStorageAPI';
 
 const basketShoppingList = document.querySelector('.basket-list');
 const start = document.querySelector('.test');
 start.addEventListener('click', renderBasketItem);
 
-async function renderBasketItem() {
-  const data = await getSelectedCategory('Paperback Nonfiction');
+export function renderBasketItem() {
+  basketShoppingList.innerHTML = '';
+
+  let data = getFromLocalStorage(refsLS.booksInCart);
   console.log(data);
+  const emptyCardContainer = document.querySelector('.js-background-container');
+  const isCardEmpty = !data || !data.length;
+  if (isCardEmpty) {
+    emptyCardContainer.classList.remove('none');
+    return;
+  }
+
   const books = data.map(markupBasketItem).join('');
   basketShoppingList.innerHTML = books;
 }
+
+async function foo() {
+  let data = await getSelectedCategory('Paperback Nonfiction');
+  data.forEach(book => {
+    addBookToCart(book);
+  });
+}
+
+// foo();
--- src/js/shopeList.js
+++ src/js/shopeList.js
@@ -1,3 +1,5 @@
 // Логіка для другої сторінки
 
 import '../js/renderBasketShList';
+
+basketShoppin;
