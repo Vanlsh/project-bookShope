f41144b189f77601bb656f1eb9a30f83114d06ca
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-02-25 21:41:47 +0200

    Fixed style of chosen nav category

--- src/js/handlerGalleryNav.js
+++ src/js/handlerGalleryNav.js
@@ -3,9 +3,11 @@ import { renderCategory } from './renders/renderCategoryBooks';
 
 export function handlerGalleryNav(evt) {
   if (evt.target.nodeName !== 'BUTTON') return;
+  const buttons = document.querySelectorAll('button[data-category]');
+  buttons.forEach(btn => btn.classList.remove('active'));
+  evt.target.classList.add('active');
 
   const attributeValue = evt.target.getAttribute('data-category');
-
   if (attributeValue === 'All categories') {
     renderBestSellerBooks();
   } else {
