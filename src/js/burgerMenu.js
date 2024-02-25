91befe0e381a2fb571d89d68597533d942f05f0a
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-02-26 00:05:20 +0200

    css update

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,14 +1,14 @@
 export function toggleMenu() {
     let menu = document.querySelector('.burger-wraper');
-    let burgerIcon = document.querySelector('.burger-icon');
+    let burgerIcon = document.querySelector('.header-open-button');
 
     if (menu.classList.contains('is-closed')) {
         menu.classList.remove('is-closed');
         menu.classList.add('is-open');
-        burgerIcon.innerHTML = 'Хрестик';
+        burgerIcon.innerHTML = '<svg class="header-close-icon" width="28" height="28"><use href="../img/icons.svg#icon-x-close"></use></svg>';
     } else {
         menu.classList.remove('is-open');
         menu.classList.add('is-closed');
-        burgerIcon.innerHTML = 'Бургер';
+        burgerIcon.innerHTML = '<svg class="header-close-icon" width="28" height="28"><use href="../img/icons.svg#icon-align-left"></use></svg>';
     }
 }
