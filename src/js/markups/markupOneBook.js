c61d4465d4d767bc1247af8d42b99e5e6a076b48
oleksasa <oleksandrasevcuk04@gmail.com>
2024-02-25 19:17:23 +0200

    complited

--- src/js/markups/markupOneBook.js
+++ src/js/markups/markupOneBook.js
@@ -1,3 +1,6 @@
+import { truncateTextByWord } from "../helpers/truncateTextByWord";
+
+
 export function markupBookCard(book) {
   const { _id: id, book_image: image, title, author } = book;
   return `
@@ -6,7 +9,7 @@ export function markupBookCard(book) {
  <img class="book-image" src="${image}" alt="${author} ${title}"/>
           <p class="book-image-message-text">QUICK VIEW</p>
 </div>
-        <h2 class="book-title">${title}</h2>
+        <h2 class="book-title">${truncateTextByWord(title)}</h2>
         <p class="book-author">${author}</p>
 </li>     
   `;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+import { getCategoryList } from "./js/api/books";
+import { renderCategory } from "./js/renders/renderCategoryBooks";
 
+
+console.log(getCategoryList());
+
+renderCategory('Business Books')
