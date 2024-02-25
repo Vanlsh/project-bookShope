3c82d108d60a86e6d283559142fd868a3e5006e9
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-02-25 19:22:32 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
-import { getCategoryList } from "./js/api/books";
-import { renderCategory } from "./js/renders/renderCategoryBooks";
 
-
-console.log(getCategoryList());
-
-renderCategory('Business Books')
