d0c6a5a0703e3269b56394ce8d29834352f9d43d
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-02-25 21:25:18 +0000

    Deploying to gh-pages from @ Vanlsh/project-bookShope@f44c2aadc5554bda3b7e5c1dbc178d3ef61a9541 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/charitableFunds-7385a04c.js";function l(t){const e=t.find(s=>s.name==="Amazon").url,a=t.find(s=>s.name==="Apple Books").url;return`
+import{g as r,r as c}from"./assets/keyConstsLS-e1ae3af3.js";function m(s){const e=s.find(a=>a.name==="Amazon").url,t=s.find(a=>a.name==="Apple Books").url;return`
     <ul class="links-shop-list">
        <li class="link-shop-item">
          <a class="amazon-link" href="${e}" target="_blank" rel="noopener">
@@ -8,7 +8,7 @@ import"./assets/charitableFunds-7385a04c.js";function l(t){const e=t.find(s=>s.n
        <li class="link-shop-item">
          <a
            class="apple-book-link"
-           href="${a}"
+           href="${t}"
            target="_blank"
            rel="noopener"
          >
@@ -19,25 +19,25 @@ import"./assets/charitableFunds-7385a04c.js";function l(t){const e=t.find(s=>s.n
            ></img>
          </a>
        </li>
-     </ul>`}function c({id:t,book_image:e,title:a,list_name:s,description:n,author:i,buy_links:r}){return`
+     </ul>`}function p({id:s,book_image:e,title:t,list_name:a,description:n,author:o,buy_links:l}){return`
     <li class="basket-item">
-      <img class="basket-item-img" src="${e}" alt="${a}"/>
+      <img class="basket-item-img" src="${e}" alt="${t}"/>
       <div class="basket-item-info">
 <div class="first-item-part">
     <div class="title-category-container">
-    <h2 class="title-basket-item">${a}</h2>
-    <p class="category-basket-item">${s}</p>
+    <h2 class="title-basket-item">${t}</h2>
+    <p class="category-basket-item">${a}</p>
     </div>
-    <button class="delete-basket-item-btn" data-id="${t}">
+    <button class="delete-basket-item-btn" data-id="${s}">
       <svg class="trash-btn-icon" height="16" width="16">
                 <use href="../img/icons.svg#icon-trash"></use>
               </svg></button>
 </div>
     <p class="descr-basket-item">${n}</p>
     <div class="last-item-part">
-    <p class="author-basket-item">${i}</p>
-${l(r)}
+    <p class="author-basket-item">${o}</p>
+${m(l)}
      </div>
      </div>
-    </li>`}const m={theme:"theme",booksInCart:"booksInCart"};function p(t){try{const e=localStorage.getItem(t);return e===null?(console.log(`The data with the key: "${t}" was not found in Local Storage`),null):JSON.parse(e)}catch(e){return console.error(`An error occurred when trying to read data from Local Starage ${e}`),null}}const o={basketShoppingList:document.querySelector(".js-basket-list"),emptyCardContainer:document.querySelector(".js-background-container"),start:document.querySelector(".js-test")};function g(){o.basketShoppingList.innerHTML="";let t=p(m.booksInCart);if(console.log(t),!t||!t.length){o.emptyCardContainer.classList.remove("none");return}const a=t.map(c).join("");o.basketShoppingList.innerHTML=a}g();
+    </li>`}const i={basketShoppingList:document.querySelector(".js-basket-list"),emptyCardContainer:document.querySelector(".js-background-container"),start:document.querySelector(".js-test")};function k(){i.basketShoppingList.innerHTML="";let s=r(c.booksInCart);if(console.log(s),!s||!s.length){i.emptyCardContainer.classList.remove("none");return}const t=s.map(p).join("");i.basketShoppingList.innerHTML=t}k();
 //# sourceMappingURL=commonHelpers2.js.map
