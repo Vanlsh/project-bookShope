cec00c9e306ddef9d30a2fb03886138786da1755
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-02-25 22:55:06 +0200

    Created function of removing class in Nav category and save to LS the name of chosen category

--- src/js/handlerGalleryNav.js
+++ src/js/handlerGalleryNav.js
@@ -1,13 +1,19 @@
 import { renderBestSellerBooks } from './renders/renderBestSellers';
 import { renderCategory } from './renders/renderCategoryBooks';
+import { removeBntNavStyles } from './helpers/removeBntNavStyles';
+import { setToLocalStorage } from './localStorageAPI';
+import { refsLS } from './keyConstsLS';
+const { categoryName } = refsLS;
 
 export function handlerGalleryNav(evt) {
   if (evt.target.nodeName !== 'BUTTON') return;
-  const buttons = document.querySelectorAll('button[data-category]');
-  buttons.forEach(btn => btn.classList.remove('active'));
+  removeBntNavStyles();
   evt.target.classList.add('active');
 
   const attributeValue = evt.target.getAttribute('data-category');
+  //save the Name of Nav Category to LS
+  setToLocalStorage(categoryName, attributeValue);
+
   if (attributeValue === 'All categories') {
     renderBestSellerBooks();
   } else {
