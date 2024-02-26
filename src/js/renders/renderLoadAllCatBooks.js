13860be524fdc05cf84867c20c4e469bb3296ef5
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-02-26 12:36:31 +0200

    Added rendering Best Sellers Books during loading web page

---
+++ src/js/renders/renderLoadAllCatBooks.js
@@ -0,0 +1,5 @@
+import { renderBestSellerBooks } from './renderBestSellers';
+const loadWindow = document.addEventListener(
+  'DOMContentLoaded',
+  renderBestSellerBooks()
+);
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './js/renders/renderNavCategoriesList';
 import { handlerGalleryNav } from './js/handlerGalleryNav';
 import { refs } from './js/refs';
-
+//Rendering bestsellers during window loading
+import './js/renders/renderLoadAllCatBooks';
+//
 const { ulCategoryContainer } = refs;
 
 ulCategoryContainer.addEventListener('click', handlerGalleryNav);
