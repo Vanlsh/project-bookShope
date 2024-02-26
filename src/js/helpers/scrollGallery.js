18b19314d726e968b836ee5a3d8beaf130a7ab22
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-02-26 15:48:51 +0200

    add function renderDefaultBestSellerBooks.

--- src/js/helpers/scrollGallery.js
+++ src/js/helpers/scrollGallery.js
@@ -1,17 +1,7 @@
 import { refs } from '../refs';
 
-// export const scrollGallery = (page, itemsInPage) => {
-//   const galleryItemPosition = refs.gallery.getBoundingClientRect();
-//   // galleryList.children[indexOfItem].getBoundingClientRect();
-//   window.scrollBy({
-//     top: 1000,
-//     // top: galleryItemPosition.y - window.innerHeight / 2,
-//     left: 0,
-//     behavior: 'smooth',
-//   });
-// };
-
 export function scrollGallery() {
+  if (window.innerWidth >= 1422) return;
   refs.gallery.style.minHeight = window.innerHeight + 'px';
   refs.gallery.scrollIntoView({
     behavior: 'smooth',
