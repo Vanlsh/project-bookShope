5838c7d32ebce12d470eaeeb137c0bfa8b0d236c
ArtemBlyshchyk <artem.bl31@gmail.com>
2024-02-26 18:56:47 +0200

    Added rendering books by chosen Category during loading web site

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,7 @@
 export const refs = {
   ulCategoryContainer: document.querySelector('#listContainer'),
   gallery: document.querySelector('.js-gallery'),
+  btnAllCategories: document.querySelector(
+    'button[data-category="All categories"]'
+  ),
 };
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './js/renders/renderNavCategoriesList';
 import { handlerGalleryNav } from './js/handlerGalleryNav';
 import { refs } from './js/refs';
 //Rendering bestsellers during window loading
-import './js/renders/renderLoadAllCatBooks';
+import { onLoadAllBestSelBooks } from './js/events/onLoadAllBestSellers';
 //
 const { ulCategoryContainer } = refs;
 
 ulCategoryContainer.addEventListener('click', handlerGalleryNav);
+
+document.addEventListener('DOMContentLoaded', onLoadAllBestSelBooks);
