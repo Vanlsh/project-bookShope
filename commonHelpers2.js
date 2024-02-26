584cd138efd73cb43aab4399dd8b2a71721cb08b
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2024-02-26 16:14:30 +0000

    Deploying to gh-pages from @ Vanlsh/project-bookShope@9600797d25eae0559d0af2bee8f83a333ab68ed4 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,43 +1,22 @@
-import{g as r,r as c}from"./assets/localStorageAPI-88cd65a5.js";function m(s){const e=s.find(a=>a.name==="Amazon").url,t=s.find(a=>a.name==="Apple Books").url;return`
-    <ul class="links-shop-list">
-       <li class="link-shop-item">
-         <a class="amazon-link" href="${e}" target="_blank" rel="noopener">
-           <img class="img-amazon" src="/img/amazon.png" alt="amazon"></img>
-         </a>
-       </li>
-       <li class="link-shop-item">
-         <a
-           class="apple-book-link"
-           href="${t}"
-           target="_blank"
-           rel="noopener"
-         >
-           <img
-             class="img-apple-books"
-             src="/img/apple-books.png"
-             alt="apple-books"
-           ></img>
-         </a>
-       </li>
-     </ul>`}function p({id:s,book_image:e,title:t,list_name:a,description:n,author:o,buy_links:l}){return`
+import{m as c,g as m,r as l}from"./assets/markupBuyLinks-25801441.js";function p({id:t,book_image:a,title:s,list_name:i,description:r,author:n,buy_links:o}){return`
     <li class="basket-item">
-      <img class="basket-item-img" src="${e}" alt="${t}"/>
+      <img class="basket-item-img" src="${a}" alt="${s}"/>
       <div class="basket-item-info">
 <div class="first-item-part">
     <div class="title-category-container">
-    <h2 class="title-basket-item">${t}</h2>
-    <p class="category-basket-item">${a}</p>
+    <h2 class="title-basket-item">${s}</h2>
+    <p class="category-basket-item">${i}</p>
     </div>
-    <button class="delete-basket-item-btn" data-id="${s}">
+    <button class="delete-basket-item-btn" data-id="${t}">
       <svg class="trash-btn-icon" height="16" width="16">
                 <use href="../img/icons.svg#icon-trash"></use>
               </svg></button>
 </div>
-    <p class="descr-basket-item">${n}</p>
+    <p class="descr-basket-item">${r}</p>
     <div class="last-item-part">
-    <p class="author-basket-item">${o}</p>
-${m(l)}
+    <p class="author-basket-item">${n}</p>
+${c(o)}
      </div>
      </div>
-    </li>`}const i={basketShoppingList:document.querySelector(".js-basket-list"),emptyCardContainer:document.querySelector(".js-background-container"),start:document.querySelector(".js-test")};function k(){i.basketShoppingList.innerHTML="";let s=r(c.booksInCart);if(console.log(s),!s||!s.length){i.emptyCardContainer.classList.remove("none");return}const t=s.map(p).join("");i.basketShoppingList.innerHTML=t}k();
+    </li>`}const e={basketShoppingList:document.querySelector(".js-basket-list"),emptyCardContainer:document.querySelector(".js-background-container"),start:document.querySelector(".js-test")};function d(){e.basketShoppingList.innerHTML="";let t=m(l.booksInCart);if(console.log(t),!t||!t.length){e.emptyCardContainer.classList.remove("none");return}const s=t.map(p).join("");e.basketShoppingList.innerHTML=s}d();
 //# sourceMappingURL=commonHelpers2.js.map
